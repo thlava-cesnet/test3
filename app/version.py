f6461d8bb3d549727d0355ccec12d15b27142f25
Tom Hlava <hlava@cesnet.cz>
2022-02-11 12:54:36 +0100

    test-220211-125436

---
+++ app/version.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#
+
+from __future__ import absolute_import, print_function
+
+__version__ = "0.0.1"
--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import find_packages, setup
 
 # Get the version string. Cannot be done with import!
 g = {}
-with open(os.path.join('test1', 'version.py'), 'rt') as fp:
+with open(os.path.join('app', 'version.py'), 'rt') as fp:
     exec(fp.read(), g)
     version = g['__version__']
 
 setup(
-    name='test1',
+    name='app',
     version=version,
     packages=find_packages()
 )
