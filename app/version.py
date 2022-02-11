71ccaffad236ebc23a7d358a1c5117226b507614
Tom Hlava <hlava@cesnet.cz>
2022-02-11 18:42:46 +0100

    test-220211-184246

--- app/version.py
+++ app/version.py
@@ -3,4 +3,4 @@
 
 from __future__ import absolute_import, print_function
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
--- setup.py
+++ setup.py
@@ -5,6 +5,25 @@ import os
 
 from setuptools import find_packages, setup
 
+from Cython.Build import cythonize
+
+EXCLUDE_FILES = [
+  'app/main.py'
+]
+
+def get_ext_paths(root_dir, exclude_files):
+    """get filepaths for compilation"""
+    paths = []
+    for root, dirs, files in os.walk(root_dir):
+        for filename in files:
+            if os.path.splitext(filename)[1] != '.py':
+                continue
+            file_path = os.path.join(root, filename)
+            if file_path in exclude_files:
+                continue
+            paths.append(file_path)
+    return paths
+
 # Get the version string. Cannot be done with import!
 g = {}
 with open(os.path.join('app', 'version.py'), 'rt') as fp:
@@ -14,5 +33,12 @@ with open(os.path.join('app', 'version.py'), 'rt') as fp:
 setup(
     name='app',
     version=version,
-    packages=find_packages()
+    entry_points={
+        'console_scripts': ['app=app.main:go'],
+    },
+    packages=find_packages(),
+    ext_modules=cythonize(
+        get_ext_paths('app', EXCLUDE_FILES),
+        compiler_directives={ 'language_level': 3 }
+    )
 )
