9a9081d1a3e68627f982f984a473147e3aac3023
Tom Hlava <hlava@cesnet.cz>
2022-02-14 16:00:35 +0100

    test-220214-160035

--- app/core.py
+++ app/core.py
@@ -4,8 +4,8 @@
 import postgresql
 
 def go():
-#    db = postgresql.open('pq://postgres:postgres@localhost:5432/testik')
-    db = postgresql.open('pq://invenio:invenio@localhost:5432/invenio')
+    db = postgresql.open('pq://postgres:postgres@localhost:5432/testik')
+#    db = postgresql.open('pq://invenio:invenio@localhost:5432/invenio')
 #    db.execute("DROP TABLE t1")
     db.execute("CREATE TABLE t1 (id integer PRIMARY KEY, txt varchar(255))")
     db.execute("INSERT INTO t1 VALUES (1, 'abc')")
